f24cab60f6849018de51fd51c1a255426b2c8c71
Lecodeur92 <95392497+Lecodeur92@users.noreply.github.com>
2021-12-02 06:08:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ typewriter
   .pauseFor(2500)
   .typeString('Développeur web - front-end')
   .pauseFor(1000)
-  .deleteChars(27)
+  .deleteAll(27)
   .typeString('<span style="color:#b62b19">Designer</span>')
   .pauseFor(1000)
-  .deleteChars(8)
+  .deleteAll(8)
   .typeString('<span style="color:#b62b19">Integrateur</span>')
   .pauseFor(1000)
   .start();
