e7ac2aea577de49209e359e1a4c4a5d7723e2c13
Lecodeur92 <95392497+Lecodeur92@users.noreply.github.com>
2021-12-02 05:29:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ typewriter
   .typeString('Développeur web - front-end')
   .pauseFor(300)
   .deleteChars(10)
-  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .typeString('Je suis un amoureux du code')
   .pauseFor(1000)
   .start();
